befaec88f5221422a457050c580915807cc57393
Christian Carlos I S <christiancarlosmngsg25@gmail.com>
2026-01-30 06:52:44 +0700

    Delete Comment.py

--- Comment.py
+++
@@ -1,5 +0,0 @@
-#python comments
-# membuat komentar harus diawali dengan tanda pagar (#), dan tidak akan dijalankan oleh python
-# ini adalah komentar saya
-print ("loscarafterdesign")
-print ("loscarafterdesign") # ini juga komentar saya
