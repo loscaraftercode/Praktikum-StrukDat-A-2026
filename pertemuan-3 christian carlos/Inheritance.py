a4b68f974f523b681ec156135de2c8815eb88604
loscaraftercode <christiancarlosmngsg25@gmail.com>
2026-02-24 21:37:56 +0700

    konsep oop

---
+++ pertemuan-3 christian carlos/Inheritance.py
@@ -0,0 +1,117 @@
+#Python Inheritance
+#Inheritance memungkinkan class mewarisi properti & method dari class lain.
+
+#Create a Parent Class
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+#Use the Person class to create an object, and then execute the printname method:
+
+x = Person("John", "Doe")
+x.printname() 
+
+#Create a Child Class
+#Buat class turunan dengan menaruh parent class sebagai parameter saat mendefinisikan child class.
+
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  pass
+
+x = Student("Mike", "Olsen")
+x.printname()
+
+
+#Add the __init__() Function
+#Kita bisa menambahkan __init__() di child class untuk inisialisasi, menggantikan pass.
+
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  def __init__(self, fname, lname):
+    Person.__init__(self, fname, lname)
+
+x = Student("Mike", "Olsen")
+x.printname()
+
+#Use the super() Function
+#super() membuat child class mewarisi semua properti & method dari parent.
+
+class Student(Person):
+  def __init__(self, fname, lname):
+    super().__init__(fname, lname) 
+
+#Add Properties
+#menambahkan properti
+
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  def __init__(self, fname, lname):
+    super().__init__(fname, lname)
+    self.graduationyear = 2019
+
+x = Student("Mike", "Olsen")
+print(x.graduationyear)
+
+#Tambahkan parameter year dan berikan nilai saat membuat objek.
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+
+x = Student("Mike", "Olsen", 2019)
+print(x.graduationyear)
+
+#Add Methods
+#menambahkan method welcome di class Student.
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+
+  def welcome(self):
+    print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
+
+x = Student("Mike", "Olsen", 2024)
+x.welcome()
+
